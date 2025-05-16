9815c716b2745ea9a5de668713dd93c1a8e25da9
Bean Can <140042296+YellowChemistryPublishing@users.noreply.github.com>
2025-05-17 00:44:38 +1000

    Commit exact sources, prefetched.

---
+++ build-i686-w64-mingw32/src/libiconv-1.17/lib/canonical_aix.h
@@ -0,0 +1,21 @@
+#ifdef _WIN32
+  (intptr_t)&((struct stringpool2_t *)0)->stringpool_aix_0,
+  (intptr_t)&((struct stringpool2_t *)0)->stringpool_aix_1,
+  (intptr_t)&((struct stringpool2_t *)0)->stringpool_aix_2,
+  (intptr_t)&((struct stringpool2_t *)0)->stringpool_aix_3,
+  (intptr_t)&((struct stringpool2_t *)0)->stringpool_aix_4,
+  (intptr_t)&((struct stringpool2_t *)0)->stringpool_aix_5,
+  (intptr_t)&((struct stringpool2_t *)0)->stringpool_aix_6,
+  (intptr_t)&((struct stringpool2_t *)0)->stringpool_aix_10,
+  (intptr_t)&((struct stringpool2_t *)0)->stringpool_aix_14,
+#else
+  (int)(long)&((struct stringpool2_t *)0)->stringpool_aix_0,
+  (int)(long)&((struct stringpool2_t *)0)->stringpool_aix_1,
+  (int)(long)&((struct stringpool2_t *)0)->stringpool_aix_2,
+  (int)(long)&((struct stringpool2_t *)0)->stringpool_aix_3,
+  (int)(long)&((struct stringpool2_t *)0)->stringpool_aix_4,
+  (int)(long)&((struct stringpool2_t *)0)->stringpool_aix_5,
+  (int)(long)&((struct stringpool2_t *)0)->stringpool_aix_6,
+  (int)(long)&((struct stringpool2_t *)0)->stringpool_aix_10,
+  (int)(long)&((struct stringpool2_t *)0)->stringpool_aix_14,
+#endif
---
+++ build-i686-w64-mingw32/src/libiconv-1.17/lib/canonical_extra.h
@@ -0,0 +1,17 @@
+#ifdef _WIN32
+  (intptr_t)&((struct stringpool2_t *)0)->stringpool_extra_0,
+  (intptr_t)&((struct stringpool2_t *)0)->stringpool_extra_2,
+  (intptr_t)&((struct stringpool2_t *)0)->stringpool_extra_4,
+  (intptr_t)&((struct stringpool2_t *)0)->stringpool_extra_6,
+  (intptr_t)&((struct stringpool2_t *)0)->stringpool_extra_7,
+  (intptr_t)&((struct stringpool2_t *)0)->stringpool_extra_9,
+  (intptr_t)&((struct stringpool2_t *)0)->stringpool_extra_11,
+#else
+  (int)(long)&((struct stringpool2_t *)0)->stringpool_extra_0,
+  (int)(long)&((struct stringpool2_t *)0)->stringpool_extra_2,
+  (int)(long)&((struct stringpool2_t *)0)->stringpool_extra_4,
+  (int)(long)&((struct stringpool2_t *)0)->stringpool_extra_6,
+  (int)(long)&((struct stringpool2_t *)0)->stringpool_extra_7,
+  (int)(long)&((struct stringpool2_t *)0)->stringpool_extra_9,
+  (int)(long)&((struct stringpool2_t *)0)->stringpool_extra_11,
+#endif
