ea5a0033e6586d7d69232ad7602e1710da2ce12e
Bean Can <140042296+YellowChemistryPublishing@users.noreply.github.com>
2025-05-16 23:39:19 +1000

    GitHub actions integration.

--- build-either-w64-mingw32-gcc.py
+++ build-either-w64-mingw32-gcc.py
@@ -1,5 +1,6 @@
 import argparse
 import os
+import subprocess
 
 parser = argparse.ArgumentParser()
 parser.add_argument(
@@ -11,10 +12,33 @@ parser.add_argument(
 )
 args = parser.parse_args()
 
-os.system(
-    (
-        f"./build --mode=gcc-15.1.0 --arch={args.arch} --enable-languages=c,c++,fortran --no-multilib --bootstrapall {'--bin-compress' if not args.noCompress else ''} {'--use-lto' if not args.noLTO else ''} "
-        "--rt-version=v12 --exceptions=dwarf --threads=posix --with-default-msvcrt=msvcrt --with-default-win32-winnt=0x0501 "
-        f' --show-subtargets --update-sources --logviewer-command=cat --wait-for-logviewer --buildroot="{cwd}/build-i686-w64-mingw32" --jobs=$(nproc)'
+cwd = os.path.dirname(os.path.realpath(__file__))
+os.chdir(cwd)
+
+assert (
+    subprocess.call(
+        (
+            f"./build --mode=gcc-15.1.0 --arch={args.arch} --enable-languages=c,c++,fortran --no-multilib --bootstrapall {'--bin-compress' if not args.noCompress else ''} {'--use-lto' if not args.noLTO else ''} "
+            "--rt-version=v12 --exceptions=dwarf --threads=posix --with-default-msvcrt=msvcrt --with-default-win32-winnt=0x0501 "
+            f' --show-subtargets --update-sources --logviewer-command=cat --wait-for-logviewer --buildroot="{cwd}/build-i686-w64-mingw32" --jobs=$(nproc)'
+        ),
+        shell=True,
     )
+    == 0
+)
+
+buildArtifacts = None
+for subdir, dirs, files in os.walk(f"build-{args.arch}-w64-mingw32"):
+    for dir in dirs:
+        if "rt_v" in dir:
+            buildArtifacts = f"{cwd}/build-{args.arch}-w64-mingw32/{dir}/mingw32"
+            break
+assert buildArtifacts is not None
+
+assert (
+    subprocess.call(
+        f"7z a {args.arch}-w64-mingw32-gcc.7z {buildArtifacts}",
+        shell=True,
+    )
+    == 0
 )
