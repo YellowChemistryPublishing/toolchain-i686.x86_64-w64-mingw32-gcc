08a7c4824753ba775d3b720562b917db3ea2c3de
Bean Can <140042296+YellowChemistryPublishing@users.noreply.github.com>
2025-05-17 23:52:07 +1000

    Maybe don't include all sources, bad idea.

--- toolchain-build/src/libiconv-1.17/lib/canonical_local.h
+++
@@ -1,7 +0,0 @@
-#ifdef _WIN32
-  (intptr_t)&((struct stringpool_t *)0)->stringpool_str193,
-  (intptr_t)&((struct stringpool_t *)0)->stringpool_str496,
-#else
-  (int)(long)&((struct stringpool_t *)0)->stringpool_str193,
-  (int)(long)&((struct stringpool_t *)0)->stringpool_str496,
-#endif
--- toolchain-build/src/libiconv-1.17/lib/canonical_osf1.h
+++
@@ -1,7 +0,0 @@
-#if _WIN32
-  (intptr_t)&((struct stringpool2_t *)0)->stringpool_osf1_0,
-  (intptr_t)&((struct stringpool2_t *)0)->stringpool_osf1_1,
-#else
-  (int)(long)&((struct stringpool2_t *)0)->stringpool_osf1_0,
-  (int)(long)&((struct stringpool2_t *)0)->stringpool_osf1_1,
-#endif
