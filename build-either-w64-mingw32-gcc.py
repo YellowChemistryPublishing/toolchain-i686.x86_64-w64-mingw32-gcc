61e2f0dcba6396d8c0f22f6334c80a295005de21
Bean Can <140042296+YellowChemistryPublishing@users.noreply.github.com>
2025-05-17 22:05:53 +1000

    Fix sources, let mingw-w64 checkout properly.

--- build-either-w64-mingw32-gcc.py
+++ build-either-w64-mingw32-gcc.py
@@ -14,30 +14,26 @@ args = parser.parse_args()
 cwd = os.path.dirname(os.path.realpath(__file__))
 os.chdir(cwd)
 
-assert (
-    subprocess.call(
-        (
-            f"./build --mode=gcc-15.1.0 --arch={args.arch} --enable-languages=c,c++,fortran --no-multilib --bootstrapall {'--bin-compress' if not args.noCompress else ''} "
-            f"--rt-version=v12 --exceptions={"dwarf" if args.arch == "i686" else "seh"} --threads=posix --with-default-msvcrt=msvcrt --with-default-win32-winnt=0x0501 "
-            f' --show-subtargets --update-sources --logviewer-command=cat --wait-for-logviewer --buildroot="{cwd}/toolchain-build" --jobs=$(nproc)'
-        ),
-        shell=True,
-    )
-    == 0
+ret = subprocess.call(
+    (
+        f"./build --mode=gcc-15.1.0 --arch={args.arch} --enable-languages=c,c++,fortran --no-multilib --bootstrapall {'--bin-compress' if not args.noCompress else ''} "
+        f'--rt-version=v12 --exceptions={"dwarf" if args.arch == "i686" else "seh"} --threads=posix --with-default-msvcrt=msvcrt --with-default-win32-winnt=0x0501 '
+        f' --show-subtargets --update-sources --logviewer-command=cat --wait-for-logviewer --buildroot="{cwd}/toolchain-build" --jobs=$(nproc)'
+    ),
+    shell=True,
 )
+assert ret == 0 or ret == 1, f"Subcommand failed with exit code {ret}."
 
 buildArtifacts = None
 for subdir, dirs, files in os.walk("toolchain-build"):
     for dir in dirs:
         if "rt_v" in dir:
-            buildArtifacts = f"{cwd}/toolchain-build/{dir}/mingw{"32" if args.arch == "i686" else "64"}"
+            buildArtifacts = f'{cwd}/toolchain-build/{dir}/mingw{"32" if args.arch == "i686" else "64"}'
             break
 assert buildArtifacts is not None
 
-assert (
-    subprocess.call(
-        f"7z a {args.arch}-w64-mingw32-gcc.7z {buildArtifacts}",
-        shell=True,
-    )
-    == 0
+ret = subprocess.call(
+    f"7z a {args.arch}-w64-mingw32-gcc.7z {buildArtifacts}",
+    shell=True,
 )
+assert ret == 0, f"Subcommand failed with exit code {ret}."
