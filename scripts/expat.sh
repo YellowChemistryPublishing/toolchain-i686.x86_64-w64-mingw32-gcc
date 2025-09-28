208ac808ee20100aa9eb36d17081637748c0e16a
starg2 <starg2@envs.net>
2025-09-28 20:05:29 +0900

    Upgrade expat to 2.7.3

--- scripts/expat.sh
+++ scripts/expat.sh
@@ -35,7 +35,7 @@
 
 # **************************************************************************
 
-PKG_VERSION=2.7.1
+PKG_VERSION=2.7.3
 PKG_NAME=expat-${PKG_VERSION}
 PKG_DIR_NAME=expat-${PKG_VERSION}
 PKG_TYPE=.tar.xz
