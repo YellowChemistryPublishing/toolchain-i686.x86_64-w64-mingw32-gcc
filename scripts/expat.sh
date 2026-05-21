c40af0a830a5ebfae271b151ce24e599cedce88a
starg2 <starg2@envs.net>
2026-05-22 05:43:49 +0900

    Upgrade expat to 2.8.1

--- scripts/expat.sh
+++ scripts/expat.sh
@@ -35,7 +35,7 @@
 
 # **************************************************************************
 
-PKG_VERSION=2.8.0
+PKG_VERSION=2.8.1
 PKG_NAME=expat-${PKG_VERSION}
 PKG_DIR_NAME=expat-${PKG_VERSION}
 PKG_TYPE=.tar.xz
